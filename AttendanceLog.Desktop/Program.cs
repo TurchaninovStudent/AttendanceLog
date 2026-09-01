662878139225a19d6b69dc46e25e75b574d3c1c0
Турчанинов Андрей Евгеньевич <689-23@std.pkgh.edu>
2026-09-01 15:41:39 +0300

    Добавить вывод названия приложения

--- AttendanceLog.Desktop/Program.cs
+++ AttendanceLog.Desktop/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Учёт посещаемости студентов AttendanceLog, Турчанинов Андрей");
