b0c005812d74171531edf773c1a71c4c4d31eba3
Турчанинов Андрей Евгеньевич <689-23@std.pkgh.edu>
2026-09-04 09:53:11 +0300

    Добавить модель и хранилище данных

---
+++ AttendanceLog.Data/Repositories/VisitRepository.cs
@@ -0,0 +1,37 @@
+﻿using AttendanceLog.Data.Models;
+
+namespace AttendanceLog.Data.Repositories;
+
+/// <summary>
+/// Репозиторий для <see cref="Visit"/>
+/// </summary>
+public class VisitRepository
+{
+    private readonly List<Visit> items =
+    [
+        new()
+        {
+            Id = 1,
+            Student = "Абрамович В.А.",
+            WasPresent = true
+        },
+        new()
+        {
+            Id = 2,
+            Student = "Мамбетов Н.А.",
+            WasPresent = true
+        },
+        new()
+        {
+            Id = 3,
+            Student = "Соколов Б.М.",
+            WasPresent = false
+        }
+    ];
+
+    /// <summary>
+    /// Получить все посещения
+    /// </summary>
+    /// <returns></returns>
+    public List<Visit> GetAll() => items;
+}
