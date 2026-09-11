5cd5066781ac0efe00fe36b7e5bfa09243ae9f09
Турчанинов Андрей Евгеньевич <689-23@std.pkgh.edu>
2026-09-11 10:33:38 +0300

    добавить выбор хранилища параметров запуска

--- AttendanceLog.Console/Program.cs
+++ AttendanceLog.Console/Program.cs
@@ -1,9 +1,25 @@
 ﻿using AttendanceLog.Data.Repositories;
+using AttendanceLog.Logic.Interfaces;
 using AttendanceLog.Logic.Services;
 
 Console.WriteLine("Учёт посещаемости студентов AttendanceLog, Турчанинов Андрей");
 
-var service = new VisitService(new VisitRepository());
+var jsonPath = Path.Combine(AppContext.BaseDirectory, "visits.json");
+var xmlPath = Path.Combine(AppContext.BaseDirectory, "visits.xml");
+var kind = args.Length > 0 ? args[0] : "json";
+IVisitRepository repository = kind switch
+{
+    "xml" => new XmlVisitRepository(xmlPath),
+    "memory" => new VisitRepository(),
+    _ => new JsonVisitRepository(jsonPath)
+};
+Console.WriteLine($"Хранилище: {kind}");
+
+var service = new VisitService(repository);
+
+//Console.Write("Посещения нового студента: ");
+//var student = Console.ReadLine() ?? "";
+//service.AddVisit(student, wasPresent: true);
 
 Console.WriteLine("Отсутствующие студенты:");
 
@@ -11,3 +27,7 @@ foreach (var item in service.GetAbsent())
 {
     Console.WriteLine($"{item.Id}: {item.Student}");
 }
+
+Console.WriteLine("Доля пропусков (%):");
+
+Console.WriteLine($"{service.GetAbsencePersentage() * 100}");
